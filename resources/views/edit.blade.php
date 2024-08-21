30b46bb60c8d2cc969834055ed2042b9a7eb27bc
IgbeAjaga <ajagaigbe144@gmail.com>
2024-08-21 11:58:01 +0100

    all done

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -3,14 +3,14 @@
 @section('content')
   
 <div class="card mt-5">
-  <h2 class="card-header text-primary" style="text-align: center";>Edit Report</h2>
+  <h2 class="card-header text-primary" style="text-align: center;">Edit Report</h2>
   <div class="card-body">
   
     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
         <a class="btn btn-primary btn-sm" href="{{ route('alloutgoing') }}"><i class="fa fa-arrow-left"></i> Back</a>
     </div>
   
-    <form action="{{ route('outgoingcalls.update',$outgoingcall->id) }}" method="POST">
+    <form action="{{ route('outgoingcalls.update', $outgoingcall->id) }}" method="POST">
         @csrf
         @method('PUT')
   
@@ -18,17 +18,17 @@
             <label for="inputBranchcalled" class="form-label"><strong>Branch Called:</strong></label>
             <select name="branchcalled" id="inputBranchcalled" class="form-select @error('branchcalled') is-invalid @enderror">
                 <option value="">Select Branch</option>
-                <option value="Asokoro">Asokoro</option>
-                <option value="Maitama">Maitama</option>
-                <option value="Garki">Garki</option>
-                <option value="Gimbiya PX">Gimbiya PX</option>
-                <option value="New Ademola">New Ademola</option>
-                <option value="Old Ademola">Old Ademola</option>
-                <option value="Old Gwarinpa">Old Gwarinpa</option>
-                <option value="New Gwarinpa">New Gwarinpa</option>
-                <option value="Gwarina 3">Gwarina 3</option>
-                <option value="Gana PX">Gana PX</option>
-                <option value="Ferma">Ferma</option>                
+                <option value="Asokoro" {{ $outgoingcall->branchcalled == 'Asokoro' ? 'selected' : '' }}>Asokoro</option>
+                <option value="Guzape" {{ $outgoingcall->branchcalled == 'Guzape' ? 'selected' : '' }}>Guzape</option>
+                <option value="New Garki" {{ $outgoingcall->branchcalled == 'New Garki' ? 'selected' : '' }}>New Garki</option>
+                <option value="Gimbiya" {{ $outgoingcall->branchcalled == 'Gimbiya' ? 'selected' : '' }}>Gimbiya</option>
+                <option value="New Ademola" {{ $outgoingcall->branchcalled == 'New Ademola' ? 'selected' : '' }}>New Ademola</option>
+                <option value="Old Ademola" {{ $outgoingcall->branchcalled == 'Old Ademola' ? 'selected' : '' }}>Old Ademola</option>
+                <option value="Gwarinpa 1" {{ $outgoingcall->branchcalled == 'Gwarinpa 1' ? 'selected' : '' }}>Gwarinpa 1</option>
+                <option value="Gwarinpa 2" {{ $outgoingcall->branchcalled == 'Gwarinpa 2' ? 'selected' : '' }}>Gwarinpa 2</option>
+                <option value="Gwarinpa 3" {{ $outgoingcall->branchcalled == 'Gwarinpa 3' ? 'selected' : '' }}>Gwarinpa 3</option>
+                <option value="Gana" {{ $outgoingcall->branchcalled == 'Gana' ? 'selected' : '' }}>Gana</option>
+                <option value="Wuse 2" {{ $outgoingcall->branchcalled == 'Wuse 2' ? 'selected' : '' }}>Wuse 2</option>                               
             </select>
             @error('branchcalled')
                 <div class="form-text text-danger">{{ $message }}</div>
@@ -53,44 +53,30 @@
             <label for="inputResponse" class="form-label"><strong>Response:</strong></label>
             <select name="response" id="inputResponse" class="form-select @error('response') is-invalid @enderror">
                 <option value="">Select Response</option>
-                <option value="in_stock">in_stock</option>
-                <option value="out_of_stock">out_of_stock</option>
-                <option value="no_response">no_response</option>              
+                <option value="in_stock" {{ $outgoingcall->response == 'in_stock' ? 'selected' : '' }}>in_stock</option>
+                <option value="out_of_stock" {{ $outgoingcall->response == 'out_of_stock' ? 'selected' : '' }}>out_of_stock</option>
+                <option value="no_response" {{ $outgoingcall->response == 'no_response' ? 'selected' : '' }}>no_response</option>              
             </select>
             @error('response')
                 <div class="form-text text-danger">{{ $message }}</div>
             @enderror
-        </div>
-
-        <div class="mb-3">
-            <label for="inputCall" class="form-label"><strong>Number of Calls:</strong></label>
-            <input 
-                type="text" 
-                name="call" 
-                value="{{ $outgoingcall->call }}"
-                class="form-control @error('call') is-invalid @enderror" 
-                id="inputCall" 
-                placeholder="Number of Calls">
-            @error('call')
-                <div class="form-text text-danger">{{ $message }}</div>
-            @enderror
-        </div>
+        </div>       
 
         <div class="mb-3">
             <label for="inputBranchthatcalled" class="form-label"><strong>Branch called from:</strong></label>
             <select name="branchthatcalled" id="inputBranchthatcalled" class="form-select @error('branchthatcalled') is-invalid @enderror">
                 <option value="">Select Branch</option>
-                <option value="Asokoro">Asokoro</option>
-                <option value="Maitama">Maitama</option>
-                <option value="Garki">Garki</option>
-                <option value="Gimbiya PX">Gimbiya PX</option>
-                <option value="New Ademola">New Ademola</option>
-                <option value="Old Ademola">Old Ademola</option>
-                <option value="Old Gwarinpa">Old Gwarinpa</option>
-                <option value="New Gwarinpa">New Gwarinpa</option>
-                <option value="Gwarina 3">Gwarina 3</option>
-                <option value="Gana PX">Gana PX</option>
-                <option value="Ferma">Ferma</option>                
+                <option value="Asokoro" {{ $outgoingcall->branchthatcalled == 'Asokoro' ? 'selected' : '' }}>Asokoro</option>
+                <option value="Guzape" {{ $outgoingcall->branchthatcalled == 'Guzape' ? 'selected' : '' }}>Guzape</option>
+                <option value="New Garki" {{ $outgoingcall->branchthatcalled == 'New Garki' ? 'selected' : '' }}>New Garki</option>
+                <option value="Gimbiya" {{ $outgoingcall->branchthatcalled == 'Gimbiya' ? 'selected' : '' }}>Gimbiya</option>
+                <option value="New Ademola" {{ $outgoingcall->branchthatcalled == 'New Ademola' ? 'selected' : '' }}>New Ademola</option>
+                <option value="Old Ademola" {{ $outgoingcall->branchthatcalled == 'Old Ademola' ? 'selected' : '' }}>Old Ademola</option>
+                <option value="Gwarinpa 1" {{ $outgoingcall->branchthatcalled == 'Old Gwarinpa' ? 'selected' : '' }}>Old Gwarinpa</option>
+                <option value="Gwarinpa 2" {{ $outgoingcall->branchthatcalled == 'Gwarinpa 2' ? 'selected' : '' }}>Gwarinpa 2</option>
+                <option value="Gwarinpa 3" {{ $outgoingcall->branchthatcalled == 'Gwarinpa 3' ? 'selected' : '' }}>Gwarinpa 3</option>
+                <option value="Gana" {{ $outgoingcall->branchthatcalled == 'Gana' ? 'selected' : '' }}>Gana</option>
+                <option value="Wuse 2" {{ $outgoingcall->branchthatcalled == 'Wuse 2' ? 'selected' : '' }}>Wuse 2</option>                
             </select>
             @error('branchthatcalled')
                 <div class="form-text text-danger">{{ $message }}</div>
